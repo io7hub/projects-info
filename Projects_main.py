f746bc3b4fd2410ed9a2e5353c30bf36c6fe1afb
Ken Kim <io7hub@naver.com>
2026-01-07 13:48:52 +0900

    sidebar

--- Projects_main.py
+++ Projects_main.py
@@ -816,7 +816,7 @@ def main():
     
     with st.sidebar:
         
-        st.title("Kⁱ⁰⁷ AI & Rule")
+        st.title("Kⁱ⁰⁷ AI & Rule Engine")
         
         search_term = st.text_input("🔎 검색", placeholder="프로젝트명, 설명, 태그...")
         
